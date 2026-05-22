370ff53abfc7aac8888becceed017d529db02c0d
amitsingh771087 <amitsingh77108792@gmail.com>
2026-05-22 15:38:28 +0530

    feat: implement sign-in and sign-up screens with input fields and OAuth integration

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,71 @@
-import React from "react";
-import { Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = async () => {};
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white ">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5 ">
+          <InputField
+            label="Name"
+            placeholder="Enter Your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter Your Email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter Your Password"
+            icon={icons.eyecross}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          {/* OAuth */}
+          <OAuth />
+
+          <Link
+            href={"/(auth)/sign-in"}
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already Have an Account ? </Text>
+            <Text className="text-primary-500">Sign In</Text>
+          </Link>
+        </View>
+        {/* Verification Model */}
+      </View>
+    </ScrollView>
   );
 };
 

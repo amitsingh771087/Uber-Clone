95a9487a7ac89c3db252230292c4228b8ae1ad83
amitsingh771087 <amitsingh77108792@gmail.com>
2026-05-22 11:57:05 +0530

    fix: trigger CodeAnt AI for review

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -13,8 +13,8 @@ const Welcome = () => {
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
-        onPress={() => router.replace("/(auth)/sign-up")}
         className="w-full flex justify-end items-end p-5"
+        onPress={() => router.replace("/(auth)/sign-up")}
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
